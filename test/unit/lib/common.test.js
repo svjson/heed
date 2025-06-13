98aaa5b168a6a5dbb8406e2f344e80fe5fe81582
Sven Johansson <sven.johansson@iteam.se>
2025-06-13 16:04:08 +0200

    Support "phases"(steps) in the .heed format

---
+++ test/unit/lib/common.test.js
@@ -0,0 +1,27 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+import { parseStyle } from '../../../lib/common.js'
+
+test.describe('parseStyles', () => {
+
+  test('parse a single style property', () => {
+    expect(parseStyle('opacity: 1;')).toEqual({ opacity: '1'});
+    expect(parseStyle('text-align: right')).toEqual({ 'text-align': 'right'});
+  });
+
+  test('parse multiple style properties', () => {
+    expect(parseStyle('text-decoration: line-through; color: #888888')).toEqual({
+      'text-decoration': 'line-through',
+      color: '#888888'
+    });
+  });
+
+  test('parse array of style properties', () => {
+    expect(parseStyle(['text-decoration: line-through', 'color: #888888'])).toEqual({
+      'text-decoration': 'line-through',
+      color: '#888888'
+    });
+  });
+
+})
--- test/unit/lib/heed-file.test.js
+++ test/unit/lib/heed-file.test.js
@@ -42,10 +42,67 @@ test('parse .heed-file with a simple text-block', async () => {
       children: [],
       content:
         'I have a song.\n' +
-        "It's really, really good.\n" +
-        "I have to sing it twice.\n" +
-        "It's understood."
+          "It's really, really good.\n" +
+          "I have to sing it twice.\n" +
+          "It's understood."
     }]
   })
 
+});
+
+
+test('parse .heed-file with a phases aside-block', async () => {
+  // Given
+  const fileData = await readAsset(import.meta.url, './assets/simple-with-phases.heed');
+
+  // When
+  const ir = parseHeedFile(fileData);
+
+  // Then
+  expect(ir).toEqual({
+    frontmatter: {
+      title: 'How to fail - main steps'
+    },
+    contents: [{
+      type: 'text',
+      depth: 0,
+      attributes: {
+        id: 'step1'
+      },
+      children: [],
+      content: '1. Trying'
+    }, {
+      type: 'text',
+      depth: 0,
+      attributes: {
+        id: 'finish',
+        style: 'color: red;'
+      },
+      children: [],
+      content: '2. FAILURE achieved!!'
+    }],
+    phases: [{
+      type: 'phase',
+      id: 'initial',
+      transitions: {}
+    }, {
+      type: 'phase',
+      id: 'trying',
+      transitions: {
+        step1: {
+          enter: { opacity: '1' },
+          rewind: { opacity: '0' }
+        }
+      }
+    }, {
+      type: 'phase',
+      id: 'failure',
+      transitions: {
+        finish: {
+          enter: { opacity: '1' },
+          rewind: { opacity: '0' }
+        }
+      }
+    }]
+  });
 });
