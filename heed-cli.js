653a81e5009358a503abf5a8401a97bf4487160c
Sven Johansson <sven.johansson@iteam.se>
2025-06-14 19:46:32 +0200

    CLI command for creating a new presentation

--- heed-cli.js
+++ heed-cli.js
@@ -1,27 +1,14 @@
 #!/bin/node
 import { Command } from 'commander';
-import path from 'path';
 import process from 'process';
-import fs from 'fs';
 
+import { runCommand } from './lib/command/command-runner.js';
+import { newPresentation } from './lib/command/new.js';
 import { findRoot } from './lib/presentation.js'
 import { showIndex } from './lib/command/show-index.js';
 import { showRoot } from './lib/command/show-root.js';
 
 const program = new Command();
-
-const runPresentationPathCommand = async (presentationRoot, command, opts) => {
-  const presentationFile = path.join(presentationRoot, 'presentation.json');
-  if (!fs.existsSync(presentationFile)) {
-    const [_, relative] = await findRoot(presentationRoot);
-    if (!relative) {
-      console.error(`No presentation.json found in '${presentationRoot}' or any of its parent directories.`);
-      process.exit(1);
-    }
-    presentationRoot = relative;
-  }
-  await command(presentationRoot, presentationFile, opts);
-}
 
 program
   .name('heed-cli')
@@ -30,12 +17,34 @@ program
 
 const showCommand = program.command('show');
 
+program
+  .command('new [presentationName] [presentationPath]')
+  .description('Create a new presentation in the current or provided folder')
+  .option('--json', 'Output index as JSON.')
+  .action(async (presentationName, presentationPath = '.', options) => {
+    await runCommand({
+      cmd: newPresentation,
+      cmdArgs: [
+        { __name: 'Presentation name', __type: 'string', __required: true, value: presentationName},
+        presentationPath
+      ],
+      cmdOpts: options
+    });
+  });
+
 showCommand
   .command('index [presentationPath]')
   .description('Display a table of slides defined in presentation.json')
   .option('--json', 'Output index as JSON.')
   .action(async (presentationPath = '.', options) => {
-    await runPresentationPathCommand(presentationPath, showIndex, { json: options.json })
+    await runCommand({
+      cmd: showIndex,
+      cmdArgs: [
+        { __type: 'root-required', value: presentationPath },
+        { __type: 'presentation-file' }
+      ],
+      cmdOpts: options
+    });
   });
 
 showCommand
@@ -43,7 +52,11 @@ showCommand
   .description('Display the root directory path of the presentation.')
   .option('--json', 'Output paths as JSON.')
   .action(async (dir = '.', options) => {
-    await showRoot(dir, options);
+    await runCommand({
+      cmd: showRoot,
+      cmdArgs: [dir],
+      cmdOpts: options,
+    });
   })
 
 program.parse(process.argv);
