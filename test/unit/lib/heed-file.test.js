51b76d6ddc3adb423f48b7a12590d4e87e634892
Sven Johansson <sven.johansson@iteam.se>
2025-06-16 20:00:43 +0200

    Keep blank lines in block content

--- test/unit/lib/heed-file.test.js
+++ test/unit/lib/heed-file.test.js
@@ -4,105 +4,132 @@ import { parseHeedFile } from '../../../lib/heed-file.js';
 
 import { readAsset } from '../../fixture.js';
 
-test('parse .heed-file with only a frontmatter header', async () => {
-  // Given
-  const fileData = await readAsset(import.meta.url, './assets/frontmatter-only.heed');
+test.describe('parseHeedFile()', () => {
 
-  // When
-  const ir = parseHeedFile(fileData);
+  test('parse .heed-file with only a frontmatter header', async () => {
+    // Given
+    const fileData = await readAsset(import.meta.url, './assets/frontmatter-only.heed');
 
-  // Then
-  expect(ir).toEqual({
-    frontmatter: {
-      id: 'header-only',
-      title: 'There is a title, though!',
-      type: 'default'
-    },
-    contents: []
+    // When
+    const ir = parseHeedFile(fileData);
+
+    // Then
+    expect(ir).toEqual({
+      frontmatter: {
+        id: 'header-only',
+        title: 'There is a title, though!',
+        type: 'default'
+      },
+      contents: []
+    });
   });
-});
 
-test('parse .heed-file with a simple text-block', async () => {
-  // Given
-  const fileData = await readAsset(import.meta.url, './assets/single-text-block.heed');
+  test('parse .heed-file with a simple text-block', async () => {
+    // Given
+    const fileData = await readAsset(import.meta.url, './assets/single-text-block.heed');
 
-  // When
-  const ir = parseHeedFile(fileData);
+    // When
+    const ir = parseHeedFile(fileData);
 
-  // Then
-  expect(ir).toEqual({
-    frontmatter: {},
-    contents: [{
-      type: 'text',
-      depth: 0,
-      attributes: {
-        id: 'main-text',
-        style: 'color: red;'
+    // Then
+    expect(ir).toEqual({
+      frontmatter: {},
+      contents: [{
+        type: 'text',
+        depth: 0,
+        attributes: {
+          id: 'main-text',
+          style: 'color: red;'
+        },
+        children: [],
+        content:
+          'I have a song.\n' +
+            "It's really, really good.\n" +
+            "I have to sing it twice.\n" +
+            "It's understood."
+      }]
+    });
+  });
+
+  test('parse .heed-file with block content containing a blank', async () => {
+    // Given
+    const fileData = await readAsset(import.meta.url, './assets/code-block.heed');
+
+    // When
+    const ir = parseHeedFile(fileData);
+
+    // Then
+    expect(ir).toEqual({
+      frontmatter: {},
+      contents: [{
+        type: 'prism:code-block',
+        depth: 0,
+        attributes: {
+          lang: 'clojure',
+          style: 'font-size: 40px'
+        },
+        children: [],
+        content:
+          '(defn evens [xs] (filter even? xs))\n' +
+            '\n' +
+            '(->> (range) evens (take 10)) ;=> (0 2 4 6 8 10 12 14 16 18)'
+      }]
+    })
+  });
+
+  test('parse .heed-file with a phases aside-block', async () => {
+    // Given
+    const fileData = await readAsset(import.meta.url, './assets/simple-with-phases.heed');
+
+    // When
+    const ir = parseHeedFile(fileData);
+
+    // Then
+    expect(ir).toEqual({
+      frontmatter: {
+        title: 'How to fail - main steps'
       },
-      children: [],
-      content:
-        'I have a song.\n' +
-          "It's really, really good.\n" +
-          "I have to sing it twice.\n" +
-          "It's understood."
-    }]
-  })
-
-});
-
-
-test('parse .heed-file with a phases aside-block', async () => {
-  // Given
-  const fileData = await readAsset(import.meta.url, './assets/simple-with-phases.heed');
-
-  // When
-  const ir = parseHeedFile(fileData);
-
-  // Then
-  expect(ir).toEqual({
-    frontmatter: {
-      title: 'How to fail - main steps'
-    },
-    contents: [{
-      type: 'text',
-      depth: 0,
-      attributes: {
-        id: 'step1'
-      },
-      children: [],
-      content: '1. Trying'
-    }, {
-      type: 'text',
-      depth: 0,
-      attributes: {
-        id: 'finish',
-        style: 'color: red;'
-      },
-      children: [],
-      content: '2. FAILURE achieved!!'
-    }],
-    phases: [{
-      type: 'phase',
-      id: 'initial',
-      transitions: {}
-    }, {
-      type: 'phase',
-      id: 'trying',
-      transitions: {
-        step1: {
-          enter: { opacity: '1' },
-          rewind: { opacity: '0' }
+      contents: [{
+        type: 'text',
+        depth: 0,
+        attributes: {
+          id: 'step1'
+        },
+        children: [],
+        content: '1. Trying'
+      }, {
+        type: 'text',
+        depth: 0,
+        attributes: {
+          id: 'finish',
+          style: 'color: red;'
+        },
+        children: [],
+        content: '2. FAILURE achieved!!'
+      }],
+      phases: [{
+        type: 'phase',
+        id: 'initial',
+        transitions: {}
+      }, {
+        type: 'phase',
+        id: 'trying',
+        transitions: {
+          step1: {
+            enter: { opacity: '1' },
+            rewind: { opacity: '0' }
+          }
         }
-      }
-    }, {
-      type: 'phase',
-      id: 'failure',
-      transitions: {
-        finish: {
-          enter: { opacity: '1' },
-          rewind: { opacity: '0' }
+      }, {
+        type: 'phase',
+        id: 'failure',
+        transitions: {
+          finish: {
+            enter: { opacity: '1' },
+            rewind: { opacity: '0' }
+          }
         }
-      }
-    }]
+      }]
+    });
   });
 });
--- test/unit/lib/tokenize.test.js
+++ test/unit/lib/tokenize.test.js
@@ -4,21 +4,43 @@ import { tokenize } from '../../../lib/tokenize.js';
 
 import { readAsset } from '../../fixture.js';
 
-test('tokenize a single text block', async () => {
-  // Given
-  const fileData = await readAsset(import.meta.url, './assets/single-text-block.heed');
+test.describe('tokenize() - content blocks', () => {
 
-  // When
-  const tokens = tokenize(fileData, 0);
+  test('tokenize a single text block', async () => {
+    // Given
+    const fileData = await readAsset(import.meta.url, './assets/single-text-block.heed');
 
-  // Then
-  expect(tokens).toEqual([
-    { type: 'blockStart', blockType: 'text', depth: 0, attrText: '{ id=main-text }', line: 0},
-    { type: 'attr', key: 'style', value: 'color: red;', depth: 0, line: 1 },
-    { type: 'content', value: "I have a song.", depth: 0, line: 2 },
-    { type: 'content', value: "It's really, really good.", depth: 0, line: 3 },
-    { type: 'content', value: "I have to sing it twice.", depth: 0, line: 4 },
-    { type: 'content', value: "It's understood.", depth: 0, line: 5 },
-    { type: 'blockEnd', depth: 0, line: 6, data: '--'}
-  ]);
-})
+    // When
+    const tokens = tokenize(fileData, 0);
+
+    // Then
+    expect(tokens).toEqual([
+      { type: 'blockStart', blockType: 'text', depth: 0, attrText: '{ id=main-text }', line: 0},
+      { type: 'attr', key: 'style', value: 'color: red;', depth: 0, line: 1 },
+      { type: 'content', value: "I have a song.", depth: 0, line: 2 },
+      { type: 'content', value: "It's really, really good.", depth: 0, line: 3 },
+      { type: 'content', value: "I have to sing it twice.", depth: 0, line: 4 },
+      { type: 'content', value: "It's understood.", depth: 0, line: 5 },
+      { type: 'blockEnd', depth: 0, line: 6, data: '--'}
+    ]);
+  });
+
+  test('tokenize a block with content containing empty lines', async () => {
+    // Given
+    const fileData = await readAsset(import.meta.url, './assets/code-block.heed');
+
+    // When
+    const tokens = tokenize(fileData, 0);
+
+    // Then
+    expect(tokens).toEqual([
+      { type: 'blockStart', blockType: 'prism:code-block', depth: 0, attrText: '{ lang=clojure }', line: 0},
+      { type: 'attr', key: 'style', value: 'font-size: 40px', depth: 0, line: 1 },
+      { type: 'content', value: '(defn evens [xs] (filter even? xs))', depth: 0, line: 2 },
+      { type: 'blank', depth: 0, line: 3 },
+      { type: 'content', value: '(->> (range) evens (take 10)) ;=> (0 2 4 6 8 10 12 14 16 18)', depth: 0, line: 4 },
+      { type: 'blockEnd', depth: 0, line: 5, data: '--'}
+    ]);
+  });
+
+});
